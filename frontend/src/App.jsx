450c30096173adc32842ad1bfcda59b8bce1f03a
lahirumenik <e19236@eng.pdn.ac.lk>
2023-08-19 19:00:25 +0530

    add react protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { blue, cyan, indigo } from '@mui/material/colors';
+import RequireAuth from './components/RequireAuth';
 
 
 
 
 function App() {
+
+  const lightTheme = createTheme({
+    palette: {
+      primary: blue,
+      secondary: indigo
+    },
+  });
   
 
   return (
-    <>
-     
-    </>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline/>
+        <Routes>
+    <Route path="/" element={<Layout />}> 
+
+        </Route>
+
+        </Routes>
+    </ThemeProvider>
   )
 }
 
